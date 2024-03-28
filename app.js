7947dd36f732aee825e6d4989150d4a4a3dfc918
PETER HOOPER <peter_hooper@hotmail.co.nz>
2024-03-28 19:08:34 +1300

    used a comma instead of a colon

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const server = http.createServer(function(req, res) {
-    res.writeHead(200, {'Content-Type', 'text/plain'});
+    res.writeHead(200, {'Content-Type': 'text/plain'});
     let message = 'ci / cd pipeline operational';
     res.end(message);
 });
