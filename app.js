36fd7eaf3a14c88ba0d53d5e8755637051669fd8
PETER HOOPER <peter_hooper@hotmail.co.nz>
2024-03-28 19:02:56 +1300

    added app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+const http = require('http');
+const server = http.createServer(function(req, res) {
+    res.writeHead(200, {'Content-Type', 'text/plain'});
+    let message = 'ci / cd pipeline operational';
+    res.end(message);
+});
+
+server.listen();
+
