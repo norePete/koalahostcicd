566181836e5906ecc35b980f2999d729694ed745
PETER HOOPER <peter_hooper@hotmail.co.nz>
2024-04-07 12:57:35 +1200

    add pay button

--- client/index.js
+++ client/index.js
@@ -1,86 +1,57 @@
 document.addEventListener('DOMContentLoaded', async () => {
-  // Load the publishable key from the server. The publishable key
-  // is set in your .env file.
-  const {publishableKey} = await fetch('/gateway/config').then((r) => r.json());
-  if (!publishableKey) {
-    addMessage(
-      'No publishable key returned from the server. Please check `.env` and try again'
-    );
-    alert('Please set your Stripe publishable API key in the .env file');
-  }
-
-  const stripe = Stripe(publishableKey, {
-    apiVersion: '2020-08-27',
-  });
-
-  // On page load, we create a PaymentIntent on the server so that we have its clientSecret to
-  // initialize the instance of Elements below. The PaymentIntent settings configure which payment
-  // method types to display in the PaymentElement.
-  const {
-    error: backendError,
-    clientSecret,
-    receipt
-  } = await fetch('/gateway/create-payment-intent').then(r => r.json());
-  if (backendError) {
-    addMessage(backendError.message);
-  }
-  addMessage(`Client secret returned.`);
-  addMessage(`${receipt}`);
-
-  // Initialize Stripe Elements with the PaymentIntent's clientSecret,
-  // then mount the payment element.
-  const elements = stripe.elements({ clientSecret });
-  const paymentElement = elements.create('payment');
-  paymentElement.mount('#payment-element');
-  // Create and mount the linkAuthentication Element to enable autofilling customer payment details
-  const linkAuthenticationElement = elements.create("linkAuthentication");
-  linkAuthenticationElement.mount("#link-authentication-element");
-  // If the customer's email is known when the page is loaded, you can
-  // pass the email to the linkAuthenticationElement on mount:
-  //
-  //   linkAuthenticationElement.mount("#link-authentication-element",  {
-  //     defaultValues: {
-  //       email: 'jenny.rosen@example.com',
-  //     }
-  //   })
-  // If you need access to the email address entered:
-  //
-  //  linkAuthenticationElement.on('change', (event) => {
-  //    const email = event.value.email;
-  //    console.log({ email });
-  //  })
-
-  // When the form is submitted...
-  const form = document.getElementById('payment-form');
-  let submitted = false;
-  form.addEventListener('submit', async (e) => {
-    e.preventDefault();
-
-    // Disable double submission of the form
-    if(submitted) { return; }
-    submitted = true;
-    form.querySelector('button').disabled = true;
-
-    const nameInput = document.querySelector('#name');
-
-    // Confirm the payment given the clientSecret
-    // from the payment intent that was just created on
-    // the server.
-    const {error: stripeError} = await stripe.confirmPayment({
-      elements,
-      confirmParams: {
-        return_url: `${window.location.origin}/gateway/client/return.html`,
+  // create payment intent
+  const payButton = document.getElementById('pay');
+  payButton.addEventListener('click', async (e) => {
+      e.preventDefault();
+      // Load the publishable key from the server. The publishable key
+      // is set in your .env file.
+      const {publishableKey} = await fetch('/gateway/config').then((r) => r.json());
+      if (!publishableKey) {
+        addMessage(
+          'No publishable key returned from the server. Please check `.env` and try again'
+        );
+        alert('Please set your Stripe publishable API key in the .env file');
       }
-    });
 
-    if (stripeError) {
-      addMessage(stripeError.message);
-
-      // reenable the form.
-      submitted = false;
-      form.querySelector('button').disabled = false;
-      return;
-    }
+      const stripe = Stripe(publishableKey, {
+        apiVersion: '2020-08-27',
+      });
+      const {
+        error: backendError,
+        clientSecret,
+        receipt
+      } = await fetch('/gateway/create-payment-intent/50').then(r => r.json());
+      if (backendError) {
+        addMessage(backendError.message);
+      }
+      addMessage(`Client secret returned.`);
+      addMessage(`${receipt}`);
+      const elements = stripe.elements({ clientSecret });
+      const paymentElement = elements.create('payment');
+      paymentElement.mount('#payment-element');
+      const linkAuthenticationElement = elements.create("linkAuthentication");
+      linkAuthenticationElement.mount("#link-authentication-element");
+      const form = document.getElementById('payment-form');
+      let submitted = false;
+      form.addEventListener('submit', async (e) => {
+        e.preventDefault();
+        if(submitted) { return; }
+        submitted = true;
+        form.querySelector('button').disabled = true;
+        const nameInput = document.querySelector('#name');
+        const {error: stripeError} = await stripe.confirmPayment({
+          elements,
+          confirmParams: {
+            return_url: `${window.location.origin}/gateway/client/return.html`,
+          }
+        });
+        if (stripeError) {
+          addMessage(stripeError.message);
+          submitted = false;
+          form.querySelector('button').disabled = false;
+          return;
+        }
+      });
   });
 
   const tempButton = document.getElementById('temp');
